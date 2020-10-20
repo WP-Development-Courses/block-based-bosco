bf0265d145cede74c452a07bb8792a8060f90c17
Fränk Klein <frank@humanmade.co.uk>
2020-10-20 17:31:54 +0200

    Catch all commit: rough draft of homepage.

--- functions.php
+++ functions.php
@@ -1,1 +1,15 @@
 <?php
+
+function bbb_enqueue_styles() {
+	wp_enqueue_style(
+		'bbb-style',
+		get_stylesheet_uri()
+	);
+}
+add_action( 'wp_enqueue_scripts', 'bbb_enqueue_styles' );
+
+function bbb_enqueue_editor_styles() {
+	add_editor_style( 'style.css');
+}
+
+add_action( 'after_setup_theme', 'bbb_enqueue_editor_styles' );
