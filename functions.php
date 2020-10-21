a4b25f66c7dc4b95d73af9706a973f24ec2d9a44
Fränk Klein <frank@humanmade.co.uk>
2020-10-21 20:51:37 +0200

    Add Normalize.css.

--- functions.php
+++ functions.php
@@ -7,8 +7,19 @@ add_action( 'after_setup_theme', 'bbb_add_theme_supports' );
 
 function bbb_enqueue_styles() {
 	wp_enqueue_style(
+		'bbb-normalize',
+		get_stylesheet_directory_uri() . '/css/normalize.css',
+		[],
+		'8.0.1'
+	);
+
+	wp_enqueue_style(
 		'bbb-style',
-		get_stylesheet_uri()
+		get_stylesheet_uri(),
+		[
+			'bbb-normalize'
+		],
+		wp_get_theme()->get( 'Version' )
 	);
 }
 add_action( 'wp_enqueue_scripts', 'bbb_enqueue_styles' );
