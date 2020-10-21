8265d3417827a100951667ebb292a81d122f3e27
Fränk Klein <frank@humanmade.co.uk>
2020-10-21 20:46:52 +0200

    Add support for featured images.

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php
 
+function bbb_add_theme_supports() {
+	add_theme_support( 'post-thumbnails' );
+}
+add_action( 'after_setup_theme', 'bbb_add_theme_supports' );
+
 function bbb_enqueue_styles() {
 	wp_enqueue_style(
 		'bbb-style',
