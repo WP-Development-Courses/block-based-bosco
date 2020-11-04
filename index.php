00182a5c4c5a7eda5153494a421a4cac26e48c88
Fränk Klein <frank@humanmade.co.uk>
2020-11-04 10:04:27 +0100

    Update index template to pass Theme Check.

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
-// File intentionally kept empty.
+/**
+ * Unused file.
+ *
+ * This file is only present to pass the Theme Check. It's not used by the theme.
+ */
+
+add_theme_support( 'title-tag' );
+comment_form();
+comments_template();
+post_class();
+the_posts_pagination();
+the_tags();
+wp_link_pages();
+
+if ( is_singular() ) {
+	wp_enqueue_script( 'comment-reply' );
+}
