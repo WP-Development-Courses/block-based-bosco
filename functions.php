bce77f515287fc0b04450865333e1be370f43f89
Fränk Klein <frank@humanmade.co.uk>
2020-12-15 17:37:57 +0100

    Use text alignment instead of CSS for Site Tagline.

--- functions.php
+++ functions.php
@@ -8,6 +8,9 @@ require_once __DIR__ . '/inc/class-require-gutenberg.php';
 function block_based_bosco_add_theme_supports() {
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'post-thumbnails' );
+
+	add_theme_support( 'experimental-custom-spacing' );
+	add_theme_support( 'custom-spacing' );
 }
 add_action( 'after_setup_theme', 'block_based_bosco_add_theme_supports' );
 
